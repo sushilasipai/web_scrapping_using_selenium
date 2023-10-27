faffcb70ccc31847c7c5b01d6a46030e2b7b7a53
sushilasipai <sushila.sipai@kotuko.it>
2023-10-26 21:33:44 -0500

    facebook json formatting done with caption

--- processFacebook.py
+++ processFacebook.py
@@ -7,6 +7,7 @@ import json
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from urllib.parse import urlparse, parse_qs
+from selenium.common.exceptions import TimeoutException  # Import TimeoutException
 
 def attempt_facebook_login(input_username, input_password, browser):
     print(input_username, input_password)
@@ -61,18 +62,29 @@ def attempt_cookie_login_facebook(url, browser):
 def perform_facebook_action(driver,data, downloader_fn, randomness_fn, line):
     try:
         error = ''
+        driver.get(data['postUrl'])
+        try:
+            element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//div[contains(@class, 'userContent')]")))
+        except TimeoutException:
+            element = None
+            print("No Caption found")
+
+        
         if(data['type'] == 'native_video' or data['type']== 'live_video_complete' ):
-            error = processVideo.downloadVideo(driver,data['postUrl'], str(int(time.time())))
+            error = processVideo.downloadVideo(driver,data['postUrl'], data['platformId'])
+            get_text_from_facebook(element, data['platformId'], f"./videos/{data['platformId']}.final.mp4")
+
         elif(data['type'] == 'video' or data['type'] == 'youtube'):
             for d in data['media']:
                 if(d['type'] == 'video'):
                     youtube = 0
                     if(data['type'] == 'youtube'):
                         youtube= 1
-                    processVideo.downloadVideo(driver,d['url'],data['platformId'] , youtube)     
+                    processVideo.downloadVideo(driver,d['url'],data['platformId'] , youtube)
+                    get_text_from_facebook(element, data['platformId'], f"./videos/{data['platformId']}.final.mp4")
+     
         else:
-            driver.get(data['postUrl'])
-            error = downloader_fn(driver, data['platformId'])
+            error = downloader_fn(driver, data['platformId'], element)
         randomness_fn(driver)
         time.sleep(random.randint(3,7))
         return error
@@ -80,14 +92,15 @@ def perform_facebook_action(driver,data, downloader_fn, randomness_fn, line):
         print(f"Error processing line: {line.strip()}")
         raise e
 
-def get_images_from_facebook_not_logged_in(driver, filename):
+def get_images_from_facebook_not_logged_in(driver, filename, caption_element):
     try:
         elements = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.XPATH, "//img[contains(@class, 'scaledImageFitWidth')]")))
-      
+
         src = elements[1].get_attribute("src")
         if(src):
             file_name = helper.convert_to_jpg(filename)
             helper.download_image(src, 'facebook', file_name )
+            get_text_from_facebook(caption_element, filename, f'./facebook/{file_name}')
         else:
             return "Not Found"
         
@@ -95,6 +108,24 @@ def get_images_from_facebook_not_logged_in(driver, filename):
     except Exception as e:
         print(f'Error: Unable to download the image {str(e)}')
         raise e
+
+def get_text_from_facebook(element, filename, file_name):
+    text = ""
+    
+    p_tags = []
+    
+    if(element != None):
+        p_tags = element.find_elements(By.TAG_NAME, 'p')
+    if(p_tags and len(p_tags) > 0):
+        for p_tag in p_tags:
+            text += p_tag.text + "\n"
+               
+    data = {f'{filename}' : text, "path" : f'{file_name}'}
+    json_data = json.dumps(data, indent=4)
+    with open('./facebook_data.jsonl', 'a') as file:
+        file.write(json_data + "\n")
+    
+
     
 def get_images_from_facebook(driver, filename):
     try:
