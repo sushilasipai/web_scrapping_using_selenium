242d4363a1e841b37f7c1f21b1e549a4b724e66f
sushilasipai <sushila.sipai@kotuko.it>
2023-11-10 11:58:55 -0600

    check false success downloads and move back to error list

---
+++ failedRowsTransferToError.py
@@ -0,0 +1,39 @@
+import json
+import numpy as np
+
+def move_data_between_jsonl_files(source_file_path, destination_file_path, reference_file_path):
+    with open(reference_file_path, 'r') as reference_file:
+        reference_entries = [json.loads(line) for line in reference_file]
+
+
+    reference_ids = {entry['platformId'] for entry in reference_entries}
+
+    moved_entries = []
+    remaining_entries = []
+
+
+    with open(source_file_path, 'r') as source_file:
+        for line in source_file:
+            entry = json.loads(line)
+            if entry['platformId'] in reference_ids:
+                moved_entries.append(entry)        
+            else:
+                remaining_entries.append(entry)
+
+
+    with open(destination_file_path, 'a') as destination_file:
+        for entry in moved_entries:
+            json.dump(entry, destination_file)
+            destination_file.write('\n')
+
+
+    with open(source_file_path, 'w') as source_file:
+        for entry in remaining_entries:
+            json.dump(entry, source_file)
+            source_file.write('\n')
+
+
+
+move_data_between_jsonl_files('./fb-covid19-frame-rel-v1_candidates_bak.jsonl', 
+                              './fb-covid19-frame-rel-v1_candidates_error.jsonl', 
+                              './failedToDownload.jsonl')
---
+++ fetchFalseDownload.py
@@ -0,0 +1,18 @@
+import os
+import json
+
+def check_images_and_log_missing(jsonl_file_path, image_directory, video_directory, log_file_path):
+    with open(jsonl_file_path, 'r') as jsonl_file, open(log_file_path, 'w') as log_file:
+        for line in jsonl_file:
+            data = json.loads(line)
+            platform_id = data.get('platformId')
+            image_exists = any(fname.startswith(platform_id) for fname in os.listdir(image_directory))
+
+            video_exists = any(fname.startswith(platform_id) and fname.endswith('.mp4') for fname in os.listdir(video_directory))
+
+            if not (image_exists or video_exists):
+                json.dump(data, log_file)
+                log_file.write('\n')
+
+
+check_images_and_log_missing('./fb-covid19-frame-rel-v1_candidates_bak.jsonl', './facebook', './facebook_videos', './failedToDownload.jsonl')
